16c1c115503f430a9c65e7ff2a85efe644d38cd6
lirenzuo <lirenzuo@qq.com>
2017-11-27 10:07:08 +0800

    RocketMQ运维指令

--- tools/src/main/java/org/apache/rocketmq/tools/command/MQAdminStartup.java
+++ tools/src/main/java/org/apache/rocketmq/tools/command/MQAdminStartup.java
@@ -72,7 +72,11 @@ import org.apache.rocketmq.tools.command.topic.UpdateTopicPermSubCommand;
 import org.apache.rocketmq.tools.command.topic.UpdateTopicSubCommand;
 import org.slf4j.LoggerFactory;
 
-public class MQAdminStartup {
+/**
+ * mqadmin启动程序
+ * RocketMQ运维指令
+ */
+ public class MQAdminStartup {
     protected static List<SubCommand> subCommandList = new ArrayList<SubCommand>();
 
     public static void main(String[] args) {
@@ -110,8 +114,10 @@ public class MQAdminStartup {
                 default:
                     SubCommand cmd = findSubCommand(args[0]);
                     if (cmd != null) {
+                        // 将main中的args转化为子命令的args（去除第一个参数）
                         String[] subargs = parseSubArgs(args);
 
+                        // 解析命令行
                         Options options = ServerUtil.buildCommandlineOptions(new Options());
                         final CommandLine commandLine =
                             ServerUtil.parseCmdLine("mqadmin " + cmd.commandName(), subargs, cmd.buildCommandlineOptions(options),
@@ -195,8 +201,11 @@ public class MQAdminStartup {
     }
 
     private static void initLogback() throws JoranException {
-        String rocketmqHome = System.getProperty(MixAll.ROCKETMQ_HOME_PROPERTY, System.getenv(MixAll.ROCKETMQ_HOME_ENV));
+       // String rocketmqHome = System.getProperty(MixAll.ROCKETMQ_HOME_PROPERTY, System.getenv(MixAll.ROCKETMQ_HOME_ENV));
 
+        String rocketmqHome = "D:\\\\eclipse-workspace\\\\rocketmq-rocketmq-all-4.1.0-incubating\\\\rocketmq-rocketmq-all-4.1.0-incubating\\\\distribution";
+
+        // 初始化Logback
         LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
         JoranConfigurator configurator = new JoranConfigurator();
         configurator.setContext(lc);
